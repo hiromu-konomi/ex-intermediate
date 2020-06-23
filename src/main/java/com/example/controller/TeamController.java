9a0bd27a6ae33bc2b4077d0e753b36d61f204e07
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-06-23 15:14:08 +0900

    リストのHTMLを作成、Teamコントローラーにlistメソッドを追加

--- src/main/java/com/example/controller/TeamController.java
+++ src/main/java/com/example/controller/TeamController.java
@@ -1,20 +1,27 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Team;
 import com.example.service.TeamService;
 
 @Controller
-@RequestMapping("/teams")
+@RequestMapping("/team")
 public class TeamController {
 
 	@Autowired
 	private TeamService service;
-	
+
 	@RequestMapping("/list")
-	public String list() {
+	public String list(Model model) {
+		List<Team> teamList = service.showList();
+		model.addAttribute("teamList", teamList);
+
 		return "team-list";
 	}
 }
